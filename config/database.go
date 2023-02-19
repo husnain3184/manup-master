bffaed65416dcea5d929df8d7f0bd7cc454bbbf3
husnain3184 <mohsingolvi@gmail.com>
2023-02-20 00:54:59 +0500

    push git

--- config/database.go
+++ config/database.go
@@ -8,9 +8,8 @@ import (
 
 func DBConnection() (*sql.DB, error) {
 	dbDriver := "mysql"
-	// dbHost := "192.168.14.2"
 	dbUser := "root"
-	dbPass := "MMp9ug6e"
+	dbPass := ""
 	dbName := "manup-master"
 	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
 	return db, err
